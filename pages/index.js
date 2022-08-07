6ae059d378b52115560dffc8b5d1575c6977de73
Haneen <haneensharar00@gmail.com>
2022-08-07 13:13:37 +0300

    hi

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
  
   return <>
-  
-<h1>Hi</h1>
+  <div style={{marginTop:'20px'}}>
+  <h1>Hi</h1>
+  </div>
+
   
   </>
 
