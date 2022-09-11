0d18af0548f0d4ac0da817963ab686f3037ea85d
Haneen <haneensharar00@gmail.com>
2022-09-11 15:41:38 +0300

    font

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link rel="prefetch" href="https://fonts.googleapis.com" as="font" />
+          <link rel="prefetch" href="https://fonts.googleapis.com" as="font" />
           <link rel="prefetch" href="https://fonts.gstatic.com" crossOrigin="true" as="font" />
-          <link rel="prefetch" href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;800;900&display=swap"  as="font" /> */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link  href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;800;900&display=swap"  rel="stylesheet"/>
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" /> */}
         </Head>
         <body>
           <Main />
